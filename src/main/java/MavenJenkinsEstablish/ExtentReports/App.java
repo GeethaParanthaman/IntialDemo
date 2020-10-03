6a3638dc98c6b11c7027d3dcb07648d27a567fcf
GeethaParanthaman <geethasarangaraj@gmail.com>
2020-10-03 11:33:00 +0530

    First code commit

---
+++ src/main/java/MavenJenkinsEstablish/ExtentReports/App.java
@@ -0,0 +1,13 @@
+package MavenJenkinsEstablish.ExtentReports;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
